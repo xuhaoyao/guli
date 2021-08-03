fec2a8ce27da75274db0baaa5b71e1ebc3740a86
xuhaoyao <623834276@qq.com>
2021-08-03 11:57:14 +0800

    补充一级分类字段

--- service/service_edu/src/main/java/com/scnu/edu/vo/CourseInfoVo.java
+++ service/service_edu/src/main/java/com/scnu/edu/vo/CourseInfoVo.java
@@ -18,6 +18,9 @@ public class CourseInfoVo {
     @ApiModelProperty(value = "课程专业ID")
     private String subjectId;
 
+    @ApiModelProperty(value = "课程专业父级ID")
+    private String subjectParentId;
+
     @ApiModelProperty(value = "课程标题")
     private String title;
 
