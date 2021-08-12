c8856f6f6986c07d75aaab1c570a493fc5f41d1c
xuhaoyao <623834276@qq.com>
2021-08-13 02:37:48 +0800

    权限控制模块

---
+++ service/service_acl/src/main/java/com/scnu/aclservice/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package com.scnu.aclservice.mapper;
+
+import com.scnu.aclservice.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
---
+++ service/service_acl/src/main/java/com/scnu/aclservice/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.scnu.aclservice.service.impl;
+
+import com.scnu.aclservice.entity.UserRole;
+import com.scnu.aclservice.mapper.UserRoleMapper;
+import com.scnu.aclservice.service.UserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
